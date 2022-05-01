7539e2ab4ec2cf8ba66ede11da2083f03ff26441
vladislav <ve@incsub.com>
2022-05-01 07:30:57 +0200

    Parse CLI flags

--- main.go
+++ main.go
@@ -1,38 +1,88 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
-const YEAR_DIFF_CUTOFF = 1000
+const yearDiffCutoff = 1000
 
 func main() {
-	useTime := false
-	tsRaw, err := strconv.Atoi(getTime())
-	if err != nil {
-		log.Fatal(err)
+	tsRaw, useTime, useTz, showHelp := getParsedArgs()
+	if showHelp {
+		fmt.Println(getHelp())
+		os.Exit(0)
 	}
-	ts := getTimestamp(tsRaw)
+
+	tsSrc, err := strconv.Atoi(tsRaw)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	ts := getTimestamp(tsSrc)
 
 	format := "2006-01-02"
 	if useTime {
-		format += "15:04:05"
+		format += "T15:04:05"
+		if !useTz {
+			ts = ts.UTC()
+		} else {
+			format += "ZZ07:00"
+		}
 	}
-	log.Println(ts.UTC().Format(format))
+	fmt.Println(ts.Format(format))
 }
 
-func getTime() string {
-	return "1653881338"    // s ts
-	return "1629484202017" // ms ts
+func getParsedArgs() (string, bool, bool, bool) {
+	showHelp := false
+	useTime := false
+	useTz := false
+	tsRaw := fmt.Sprintf("%d", time.Now().Unix())
+
+	for _, arg := range os.Args {
+		switch strings.Trim(arg, "-") {
+		case "t":
+			if useTime {
+				useTz = true
+			}
+			useTime = true
+		case "tt":
+			useTime = true
+			useTz = true
+		case "h":
+			showHelp = true
+		default:
+			tsRaw = arg
+		}
+	}
+
+	return tsRaw, useTime, useTz, showHelp
+	// return "1653881338"    // s ts
+	// return "1629484202017" // ms ts
+}
+
+func getHelp() string {
+	tab := "\t"
+	help := []string{
+		"USAGE:",
+		fmt.Sprintf("%s%s [-tth] TIMESTAMP", tab, os.Args[0]),
+		"... where TIMESTAMP is a numeric string with UNIX timestamp in (milli)seconds",
+		"OPTIONS:",
+		fmt.Sprintf("%s-t: Show time (default: false).", tab),
+		fmt.Sprintf("%s    If doubled up, also convert to local and show TZ (default: UTC)", tab),
+		fmt.Sprintf("%s-h: Show help", tab),
+	}
+
+	return strings.Join(help, "\n")
 }
 
 func getTimestamp(tsRaw int) time.Time {
 	tsS := time.Unix(int64(tsRaw), 0)
 	tsMs := time.UnixMilli(int64(tsRaw))
-	log.Println(tsS.Year(), tsMs.Year(), tsS.Year()-tsMs.Year())
-	if tsS.Year()-tsMs.Year() > YEAR_DIFF_CUTOFF {
+	if tsS.Year()-tsMs.Year() > yearDiffCutoff {
 		return tsMs
 	}
 	return tsS
